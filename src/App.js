1aabb929445a2f4682bade9b904c86c51bf31bf9
100pecheneK <mistermihail23@gmail.com>
2021-03-31 20:28:36 +0500

    reinit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, useReducer } from 'react'
+import TodoList from './TodoList'
+import { Context } from './context'
+import reducer from './reducer'
+
+export default function App() {
+  const todos = localStorage.getItem('todos')
+  const [state, dispatch] = useReducer(reducer, todos ? JSON.parse(todos) : [])
+
+  const [todoTitle, setTodoTitle] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(state))
+  }, [state])
+
+  const addTodo = e => {
+    if (e.key === 'Enter') {
+      dispatch({
+        type: 'ADD',
+        payload: todoTitle
+      })
+      setTodoTitle('')
+    }
+  }
+
+
+  return (
+    <Context.Provider value={{
+      dispatch
+    }}>
+      <div className="container">
+        <h1>Todo app</h1>
+
+        <div className="input-field">
+          <input
+            type="text"
+            value={todoTitle}
+            onChange={e => setTodoTitle(e.target.value)}
+            onKeyPress={addTodo}
+          />
+          <label>Todo name</label>
+        </div>
+
+        <TodoList todos={state} />
+      </div>
+    </Context.Provider>
+  )
+
+}
